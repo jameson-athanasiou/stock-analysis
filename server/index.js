09e0e1dde27756e17e39553ba5beffcb95a32c91
Jameson Athanasiou <jamesonathanasiou@gmail.com>
2021-02-19 21:08:32 -0500

    temp comment out star route

--- server/index.js
+++ server/index.js
@@ -45,9 +45,9 @@ if (!isProd){
 
 app.use(express.static(path.join(__dirname, './dist')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+// })
 
 const PORT = process.env.PORT || '3000'
 
