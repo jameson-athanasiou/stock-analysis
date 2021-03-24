a29cd2f139d0d5ff9d4b51b41fd6071d0d7ae4ec
Jameson Athanasiou <jamesonathanasiou@gmail.com>
2021-03-23 21:44:41 -0400

    return statistics to client correctly

--- server/services/stockAnalysis/access.js
+++ server/services/stockAnalysis/access.js
@@ -2,7 +2,7 @@ const { getWebPage } = require('../../util/webPage')
 const mapResponse = require('./mapper/response')
 
 const getStatistics = async (ticker) => {
-  const url = `hhttps://stockanalysis.com/stocks/${ticker.toLowerCase()}/statistics`
+  const url = `https://stockanalysis.com/stocks/${ticker.toLowerCase()}/statistics`
 
   const page = await getWebPage(url)
   await page.waitForSelector('table:nth-of-type(2)', { timeout: 5000 })
